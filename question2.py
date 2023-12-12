dfbf47a0c76d4e879b4aa3166e3b31a4b336fe79
Agry88 <rgok307085@gmail.com>
2023-12-12 17:14:36 +0800

    feat: complete question2

--- question2.py
+++ question2.py
@@ -4,10 +4,14 @@ def Question2():
     print("Question 2: ")
     data = dataset.Dataset()
 
-    # Assuming 'groupby' and 'sum' methods are available in your Dataset class
-    grouped_data = data.groupby("城市").sum("總收入")
+    # Assuming 'groupby', 'sum', 'sort_values' methods are available in your Dataset class
+    grouped_data = data.groupby("城市").agg({"總收入": "sum", "總費用": "sum", "總退款": "sum"})
+    sorted_data = grouped_data.sort_values(by="總收入", ascending=False)
 
-    print("Grouped Data:")
-    print(grouped_data)
+    # Display only the top third rows with specific columns
+    result = sorted_data.head(3)[["總收入", "總費用", "總退款"]]
+
+    print("Top Third Data:")
+    print(result)
 
 Question2()
