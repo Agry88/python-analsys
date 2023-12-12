772c1ba991d917a26b99cddead1e500ae2e12121
Agry88 <rgok307085@gmail.com>
2023-12-12 23:27:34 +0800

    fix: fill nan with variable instead delete it.

--- dataset.py
+++ dataset.py
@@ -19,8 +19,16 @@ def Dataset():
 
     data = pd.read_csv(file_path, encoding=result['encoding'])
 
-    # Drop the Nan data
-    data = data.dropna()
+    # 對數值型資料進行中位數填補
+    numeric_columns = data.select_dtypes(include=['int64', 'float64']).columns
+    for column in numeric_columns:
+        data[column].fillna(data[column].median(), inplace=True)
+
+    # 對類別型資料進行眾數填補
+    category_columns = data.select_dtypes(include='object').columns
+    for column in category_columns:
+        mode_value = data[column].mode()[0]
+        data[column].fillna(mode_value, inplace=True)
 
     # 顯示資料
     print("資料:")
