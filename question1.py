0df394416937c00b681aa44c96dcdd3ee3e780b7
Agry88 <rgok307085@gmail.com>
2023-12-12 17:06:48 +0800

    feat: add EDA for question1.

--- question1.py
+++ question1.py
@@ -1,8 +1,17 @@
-import dtale
+import sweetviz as sv
+import dataset
+
 import dataset
 def Question1():
     print("探索性分析")
-    dtale.show(dataset.Dataset())
+
+    data = dataset.Dataset()
+
+    #EDA using Autoviz
+    sweet_report = sv.analyze(data)
+
+    #Saving results to HTML file
+    sweet_report.show_html('sweet_report.html')
 
 
 Question1()
